b37a27aec8b85620b3ce03431a324f53398658d9
jsv-ecitronica <javier.soto@escuelaing.edu.co>
2022-10-02 00:01:34 +0100

    Adición de ejemplos con temporizadores, modos polling e interrupción.

--- STM32CubeIDEworkspace_1.10.1/Adivinanzas1a5/Readme.txt
+++ STM32CubeIDEworkspace_1.10.1/Adivinanzas1a5/Readme.txt
@@ -4,7 +4,7 @@ Instrucciones para Implementar el juego de as adivinanzas. Para este ejemplo se 
 
 2- Cambie la etiqueta del led (o uno de los leds, dependiendo de la tarjeta que use) a "LedGanador". El resto de periféricos usará la configuración por defecto, sin embargo asegurese de que el SysTick (tiempo base) y que la USART2 (115200-8N1) estén activas.   
 
-3- El foro de Shawn Hymel presenta el código que se usara en este proyecto. Siga las instrucciones de dicho foro. A continuación se presentan las instrucciones de manera resumida:
+3- El foro de Shawn Hymel presenta el código que se usará en este proyecto. Siga las instrucciones de dicho foro. A continuación se presentan las instrucciones de manera resumida:
 
 >> Deshabilite el archivo "syscalls.c": Click derecho sobre el archivo -> Properties -> C/C++ Build -> Seleccione "Exclude Resources form build" -> Apply and Close.
 
@@ -16,5 +16,11 @@ Instrucciones para Implementar el juego de as adivinanzas. Para este ejemplo se 
 
 5- Copie y pegue las secciones de codigo correspondientes desde el archivo "main.c"
  
+*******************************************************************************
+Nota: dependiendeo del dispositivo (microcontrolador/tarjeta nucleo) que esté usando, es posible que deba realizar las siguientes modificaciones en el código:
+
+>> Referenciar una uart diferente para la función "RetargetInit". Para este ejemplo se utilizó la uart2 (huart2).
+
+>> Especificar la libreria HAL del dispositivo correspondiente en el archivo "retarget.h". Para este ejemplo se utilizó: #include "stm32f1xx_hal.h". La ruta de acceso para verificar el nombre de la librería en este caso es: "\Drivers\STM32F1xx_HAL_Driver\Inc\stm32f1xx_hal.h"
 
   
---
+++ STM32CubeIDEworkspace_1.10.1/TimerTest_v01_ToogleLed_IRQ/Core/Src/main_TimerTest_v01_ToogleLed_IRQ.c
@@ -0,0 +1,426 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  *
+  * Ejemplo de uso del Temporizador de propósito general TIM3 - Interrupt mode
+  *
+  * Slave Mode:         Disable
+  * Trigger Source:     Disable
+  * Clock Source:       Internal Clock
+  * CH1 to CH4:         Disable
+  * Combined Channels:  Disable
+  * One Pulse Mode:     No
+  *
+  * Parameter Settings/Counter Settings:
+  * Preescaler:                     64000-1
+  * Counter Mode:                   Up
+  * Counter Period:                 1000-1
+  * Internal Clock Division (CKD):  No division
+  * Auto-reload preload:            Disable
+  *
+  * TIM2 Global Interrup: Enabled
+  * Preemption Priority: 0, Sub Priority: 0
+  *
+  * APB1 Timers Clocks: 64 MHz
+  *
+  * TIM2 Frecuencia conteo:         (64MHz/(64000)) = 1KHz
+  * TIM2 Frecuencia desbordamiento: (64MHz/(64000 * 1000)) = 1Hz
+  * TIM2 Periodo desbordamiento:    1 secs
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+/* USER CODE END Includes */
+#include <stdio.h>
+#include <string.h>
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim3;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+char uartBuf[50];
+int uartBufLenght;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_TIM3_Init(void);
+/* USER CODE BEGIN PFP */
+void print(char* text);
+void printLine(char* text);
+void printTextValue(char* text, uint16_t value);
+void printValue(uint16_t value);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_TIM3_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  print("\x1b[2J"); //Clear the screen
+  printLine("***************************************************************");
+  printLine("* Universidad Escuela Colombiana de Ingenieria Julio Garavito *");
+  printLine("* DMIC - Javier Soto - 2022                                   *");
+  printLine("* Temporizador Timer 3 - TIM3 - Interrupt mode (modo IRQ)     *");
+  printLine("* Desarrollado para una tarjeta NUCLE0-F103RB                 *");
+  printLine("***************************************************************");
+  printLine("");
+
+  printLine("LED OFF");
+  printLine("Timer:");
+
+  HAL_TIM_Base_Start_IT(&htim3);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 64000-1;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 1000-1;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    //
+    static uint8_t secs = 0;
+    //
+    if (htim == &htim3) {
+        char buf[10];
+        //
+        if (++secs >= 10) {
+          //
+          HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+          //
+          printValue(secs); //imprime el 10 y luego el estado del LED
+          //
+          char* str = (HAL_GPIO_ReadPin(LD2_GPIO_Port, LD2_Pin)==GPIO_PIN_SET)? "ON":"OFF";
+          strcpy(buf, ", LED ");
+          print(strcat(buf, str));
+          //
+          printLine("Timer:"); //mensaje de inicio de nueva cuenta, segundo 0
+          //
+          secs = 0;
+        } else {
+            printValue(secs);
+        }
+
+//        if (__HAL_TIM_GET_FLAG(&htim2, TIM_FLAG_UPDATE) != RESET) {
+//              __HAL_TIM_CLEAR_FLAG(&htim2, TIM_FLAG_UPDATE); //Borra la bandera de interrupcion del timer.
+//              //
+//              HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+//              //
+//              printValue(++secs); //imprime el 10 y luego el estado del LED
+//              //
+//              char* str = (HAL_GPIO_ReadPin(LD2_GPIO_Port, LD2_Pin)==GPIO_PIN_SET)? "ON":"OFF";
+//              strcpy(buf, ", LED ");
+//              print(strcat(buf, str));
+//              //
+//              printLine("Timer:"); //mensaje de inicio de nueva cuenta, segundo 0
+//              //
+//              secs = 0;
+//              timerLimit = 1000;
+//              //
+//            } else if (__HAL_TIM_GET_COUNTER(&htim2) >= timerLimit) {
+//              timerLimit += 1000;
+//              printValue(++secs);
+//            }
+
+
+
+
+
+    }
+
+}
+
+void print(char* text) {
+    char uartBuf[100];
+    int uartBufLenght;
+    //
+    uartBufLenght = snprintf(uartBuf, sizeof(uartBuf), "%s", text);
+    HAL_UART_Transmit(&huart2, (uint8_t*)uartBuf, uartBufLenght, 100);
+}
+
+void printLine(char* text) {
+    char uartBuf[100];
+    int uartBufLenght;
+    //
+    uartBufLenght = snprintf(uartBuf, sizeof(uartBuf), "\n\r%s", text);
+    HAL_UART_Transmit(&huart2, (uint8_t*)uartBuf, uartBufLenght, 100);
+}
+
+void printTextValue(char* text, uint16_t value) {
+    char uartBuf[100];
+    int uartBufLenght;
+    //
+    uartBufLenght = snprintf(uartBuf, sizeof(uartBuf), "%s: %u", text, value);
+    HAL_UART_Transmit(&huart2, (uint8_t*)uartBuf, uartBufLenght, 100);
+}
+
+void printValue(uint16_t value) {
+    char uartBuf[100];
+    int uartBufLenght;
+    //
+    uartBufLenght = snprintf(uartBuf, sizeof(uartBuf), " %u", value);
+    HAL_UART_Transmit(&huart2, (uint8_t*)uartBuf, uartBufLenght, 100);
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
